05537e6ab5716df0938a922b8bf3008fe00b670a
Martin N. <mr.avgp@googlemail.com>
2014-02-04 08:11:01 +0100

    Maybe the callbacks have been missing?

--- index.js
+++ index.js
@@ -10,6 +10,35 @@ function receiverListener(e) {
   }
 }
 
+function sessionListener(s) {
+  console.log("Session received", s); 
+}
+
+function onInitSuccess() {
+  var button = document.getElementById("startCast");
+  button.style.backgroundColor = "green";
+  
+  button.addEventListener("click", function() {
+    chrome.cast.requestSession(function success(session) {
+      session.sendMessage("urn:x-cast:remotedom", "Ohai!", function sent() {
+        alert("Yay");
+      }, function failed() {
+        alert("Oh no, can't talk to the Chrome Cast :(");
+      })
+    }, function error(e) {
+      alert("Oh no, it didn't work :(");
+      console.log(e);
+    });
+  }, false);
+}
+
+function onError = function(err) {
+  alert("Shizzle - the initialization failed :(");
+  console.log(err);
+  
+  document.getElementById("startCast").style.backgroundColor = "red";
+}
+
 var initializeCastApi = function() {
   var sessionRequest = new chrome.cast.SessionRequest("9A0FCE32");
   var apiConfig = new chrome.cast.ApiConfig(sessionRequest,
@@ -17,16 +46,3 @@ var initializeCastApi = function() {
     receiverListener);
   chrome.cast.initialize(apiConfig, onInitSuccess, onError);
 };
-
-document.getElementById("startCast").addEventListener("click", function() {
-  chrome.cast.requestSession(function success(session) {
-    session.sendMessage("urn:x-cast:remotedom", "Ohai!", function sent() {
-      alert("Yay");
-    }, function failed() {
-      alert("Oh no, can't talk to the Chrome Cast :(");
-    })
-  }, function error(e) {
-    alert("Oh no, it didn't work :(");
-    console.log(e);
-  });
-}, false);
